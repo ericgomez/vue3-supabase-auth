0c5d14b2c1a30c8650e4f3076b742d79e7e1f42b
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-12-14 16:53:14 -0600

    Iniciar sesión

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,6 +3,10 @@ import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import LoginForm from '@/components/Auth/LoginForm.vue';
 
+import { supabase } from "@/utils/supabase";
+import { setLoading } from "@/utils/loading";
+import { setNotification } from "@/utils/notification";
+
 export default {
   name: 'LoginView',
   components: {
@@ -12,9 +16,34 @@ export default {
     const router = useRouter();
 
     const email = ref('app@gmail.com');
-    const password = ref('prueba123');
+    const password = ref('password');
 
-    const login = async () => {};
+    const login = async () => {
+      try {
+        setLoading(true);
+
+        const { error } = await supabase.auth.signIn({
+          email: email.value,
+          password: password.value,
+        });
+
+        if (error) throw error;
+
+        await router.push("/profile");
+
+        setNotification({
+          title: "¡Bienvenid@",
+          message: "¡Hola de nuevo!"
+        });
+      } catch (e) {
+        setNotification({
+          title: "Error autenticando",
+          message: e.message,
+        });
+      } finally {
+        setLoading(false);
+      }
+    };
 
     return {
       email,
