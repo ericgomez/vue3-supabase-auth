c83b9147d3b760a09ef961c0d9a3718f25011c88
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-12-14 16:39:14 -0600

    Registrar usuarios

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -3,6 +3,10 @@ import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import SignupForm from '@/components/Auth/SignupForm.vue';
 
+import { supabase } from "@/utils/supabase";
+import { setLoading } from "@/utils/loading";
+import { setNotification } from "@/utils/notification";
+
 export default {
   name: 'SignupView',
   components: { SignupForm },
@@ -12,7 +16,44 @@ export default {
     const email = ref('app@gmail.com');
     const password = ref('password');
 
-    const signup = async () => {};
+    const signup = async () => {
+      try {
+        setLoading(true);
+
+        const { data } = await supabase
+            .from('profiles')
+            .select('id')
+            .eq('username', email.value)
+            .single()
+
+        if(data) {
+          throw Error('¡El usuario ya está registrado!');
+        }
+
+        const { error } = await supabase.auth.signUp({
+          email: email.value,
+          password: password.value
+        });
+
+        if(error) throw error;
+
+        // ok
+        await router.push('/profile');
+
+        setNotification({
+          title: 'Cuenta creada',
+          message: '¡Tu cuenta se ha creado con éxito! Revisa tu correo y confirma'
+        });
+
+      } catch (e) {
+        setNotification({
+          title: '¡Error creando cuenta!',
+          message: e.message
+        });
+      } finally {
+        setLoading(false);
+      }
+    };
 
     return {
       email,
